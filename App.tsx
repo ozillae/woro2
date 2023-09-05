90deb3d2c1e0592218837af68c0e916dca8f57f1
ozillae <ofauzi49@yahoo.co.id>
2023-09-05 22:33:10 +0700

    start up receiver

--- App.tsx
+++ App.tsx
@@ -1,18 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
 // import RNAutostart from 'react-native-autostart';
 import AutoStart from 'react-native-autostart';
 
+const getDataDevice = `(function() {
+  const tokenLocalStorage = window.localStorage.getItem('device');
+  console.log('Token push ', tokenLocalStorage);
+  window.ReactNativeWebView.postMessage(tokenLocalStorage);
+})();`;
+
 const App = () => {
-    // RNAutostart.open();
     if(AutoStart.isCustomAndroid()) {
         AutoStart.startAutostartSettings();
+    }
+
+    function onMessage333(data) {
+        let deviceCode = data?.nativeEvent?.data
+        console.log('Device Code ' + deviceCode);
     }
 
     return <WebView source={{ uri: 'https://reklame.purwosejati.com/display' }}
           style={{ flex: 1 }}
           mediaPlaybackRequiresUserAction={false}
+          injectedJavaScript={getDataDevice}
+          onMessage={onMessage333}
           />;
 }
 
--- android/app/src/main/java/com/woro2/StartupOnBootUpReceiver.java
+++ android/app/src/main/java/com/woro2/StartupOnBootUpReceiver.java
@@ -3,6 +3,7 @@ package com.woro2;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class StartupOnBootUpReceiver extends BroadcastReceiver {
 
@@ -11,6 +12,7 @@ public class StartupOnBootUpReceiver extends BroadcastReceiver {
             Intent activityIntent = new Intent(context, MainActivity.class);
             activityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             context.startActivity(activityIntent);
+            Log.i("Mytag", "------ tried to launch MainActivity -------");
         }
     }
 }
